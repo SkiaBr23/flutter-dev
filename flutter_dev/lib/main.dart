9be1b867cdfc8a78c718799b32243562dd6e5e04
Rafael Costa <rafaelcosta.rdc@hotmail.com>
2019-11-14 16:09:14 -0300

    Testing name generator

--- flutter_dev/lib/main.dart
+++ flutter_dev/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import "dart:math";
 
 void main() => runApp(MyApp());
 
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Title Home'),
+      home: MyHomePage(title: 'Teste'),
     );
   }
 }
@@ -44,16 +45,34 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String _nome = "";
+  String _motivo = "";
+  final _random = new Random();
 
-  void _incrementCounter() {
+  final List nomes = ["Consagrado", "Pontíficie", "Melindroso", "Diplomata",
+  "Supra-sumo", "Influencer", "Tributarista", "Promotor", "Patrão",
+  "Peregrino", "Maestro", "Litorâneo", "Amoroso", "Piloto",
+  "Palestrinha", "Jesuíta", "Zagueiro", "Combatente"];
+  final List motivos = ["que hoje ela me bloqueou no zap",
+    "que amanhã é feriado no Acre",
+  "que o vale-refeição caiu",
+  "que o Mengão ganhou mais uma",
+  "que hoje eu vou capotar o Corsa",
+  "que a Original 600 tá trincando",
+  "que o pagodão começa 18h"];
+
+  String gerarMotivo(){
+    return motivos[_random.nextInt(motivos.length)];
+  }
+
+  String gerarNome(){
+    return nomes[_random.nextInt(nomes.length)];
+  }
+
+  void _updateScreen() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _nome = gerarNome();
+      _motivo = gerarMotivo();
     });
   }
 
@@ -67,42 +86,40 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Opa, traz mais uma meu',
+              style: TextStyle(
+                fontSize: 35.0,
+                fontWeight: FontWeight.w500,
+                color:Colors.black87,
+              ),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+              '$_nome',
+              style: TextStyle(
+              fontSize: 50.0,
+              color:Colors.indigo,
+            ),
+            ),
+            Text(
+              '$_motivo',
+              style: TextStyle(
+                fontSize: 35.0,
+                fontWeight: FontWeight.w500,
+                color:Colors.black87,
+              ),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _updateScreen,
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
